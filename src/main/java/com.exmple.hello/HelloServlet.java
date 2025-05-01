519ad754ca9af33ad70cca00336467a82d1875f0
Chetan V. Shah <79932411+chetanvshah20@users.noreply.github.com>
2025-05-01 10:43:57 +0530

    Update HelloServlet.java

--- src/main/java/com.exmple.hello/HelloServlet.java
+++ src/main/java/com.exmple.hello/HelloServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello")
+@WebServlet(name="hello",urlPatterns={"/hello"})
 public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
